8d87613c6f11b5b4e45f5a2321bd22727fa071c0
Shilat Sharon <shisharo@g.jct.ac.il>
2023-09-10 15:47:57 +0300

    addinf functions in Home controller and views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using WebApplicationIceCreamProject.Data;
 using WebApplicationIceCreamProject.Models;
 
 namespace WebApplicationIceCreamProject.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        //private readonly ILogger<HomeController> _logger;
+        private readonly AdminContext _context;
+        public HomeController(/*ILogger<HomeController> logger*/AdminContext context)
         {
-            _logger = logger;
+            //_logger = logger;
+            _context=context;   
         }
 
         public IActionResult Index()
         {
             return View();
         }
+        // GET: Home/Shop
+        public async Task<IActionResult> Shop()
+        {
+            return _context.IceCream != null ?
+                        View(await _context.IceCream.ToListAsync()) :
+                        Problem("Entity set 'AdminContext.IceCream'  is null.");
+        }
+        // GET: Home/About
+        public IActionResult About()
+        {
+            return View();
+        }
 
+        // GET: Home/Contact
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        // GET:Home/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.IceCream == null)
+            {
+                return NotFound();
+            }
+
+            var iceCream = await _context.IceCream
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (iceCream == null)
+            {
+                return NotFound();
+            }
+
+            return View(iceCream);
+        }
         public IActionResult Privacy()
         {
             return View();
