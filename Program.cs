7d09afd70561a615f74839f9b76598ea520684ad
Leli Drach <drach@g.jct.ac.il>
2023-09-11 18:54:40 +0300

    ADDING A LINE TO THE PROGRAM.CS

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using WebApplicationIceCreamProject.Data;
 using Microsoft.Extensions.DependencyInjection;
 
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<IceCreamContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AdminContext") ?? throw new InvalidOperationException("Connection string 'AdminContext' not found.")));
@@ -16,7 +17,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-
+// Add session support
+builder.Services.AddSession(); // Add this line
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,9 +32,11 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+//builder.Services.AddScoped<ShoppingCartActions>();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+// Add session middleware
+app.UseSession(); // Add this line
 
 app.UseRouting();
 
