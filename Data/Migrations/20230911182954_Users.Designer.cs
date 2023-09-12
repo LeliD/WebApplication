cacccb33b9078c9d9b00f0b5b033ade1ba408dc7
Leli Drach <drach@g.jct.ac.il>
2023-09-12 16:16:00 +0300

    adding user- log in and register options

--- Data/Migrations/20230911182954_Users.Designer.cs
+++ Data/Migrations/20230911182954_Users.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplicationIceCreamProject.Data;
 
@@ -11,9 +12,10 @@ using WebApplicationIceCreamProject.Data;
 namespace WebApplicationIceCreamProject.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230911182954_Users")]
+    partial class Users
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
