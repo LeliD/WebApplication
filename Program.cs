369cbafd660d84124b96f44f11d17a6fabcbe969
Shilat Sharon <shisharo@g.jct.ac.il>
2023-09-22 01:48:05 +0300

    program.cs start controller

--- Program.cs
+++ Program.cs
@@ -47,7 +47,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Admin}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
